9e7d2fa966936e024994a90558406fb9ae34eeeb
Koen de Laat <koen.de.laat@philips.com>
2019-06-28 14:53:56 +0200

    Tag default ACL, SG and RT

--- main.tf
+++ main.tf
@@ -2,13 +2,75 @@ terraform {
   required_version = ">= 0.8"
 }
 
-
 resource "aws_vpc" "vpc" {
   cidr_block           = "${cidrsubnet(var.cidr_block, 0, 0)}"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = "${merge(map("Name", format("%s-vpc", var.environment)),
+          map("Environment", format("%s", var.environment)),
+          map("Project", format("%s", var.project)),
+          var.tags)}"
+}
+
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = "${aws_vpc.vpc.default_network_acl_id}"
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = "${merge(map("Name", format("%s-acl", var.environment)),
+          map("Environment", format("%s", var.environment)),
+          map("Project", format("%s", var.project)),
+          var.tags)}"
+
+  lifecycle {
+    ignore_changes = ["subnet_ids"]
+  }
+}
+
+resource "aws_default_route_table" "route_table" {
+  default_route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+
+  tags = "${merge(map("Name", format("%s-default-routetable", var.environment)),
+          map("Environment", format("%s", var.environment)),
+          map("Project", format("%s", var.project)),
+          var.tags)}"
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${merge(map("Name", format("%s-default-sg", var.environment)),
           map("Environment", format("%s", var.environment)),
           map("Project", format("%s", var.project)),
           var.tags)}"
@@ -116,9 +178,9 @@ resource "aws_eip" "nat" {
   vpc   = true
 
   tags = "${merge(map("Name", format("%s-eip", var.environment)),
-          map("Environment", format("%s", var.environment)),
-          map("Project", format("%s", var.project)),
-          var.tags)}"
+           map("Environment", format("%s", var.environment)),
+           map("Project", format("%s", var.project)),
+           var.tags)}"
 }
 
 resource "aws_nat_gateway" "nat" {
