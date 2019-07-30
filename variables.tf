5c1c20fef13742dca37f92e7888752130a282fb9
Niek Palm <dev.npalm@gmail.com>
2019-07-30 18:42:28 +0200

    Upgrade module to terraform 0.12

--- variables.tf
+++ variables.tf
@@ -1,62 +1,58 @@
 variable "environment" {
-  type = "string"
+  description = "Environment name, will be added for resource tagging."
+  type        = string
 }
 
 variable "project" {
-  type    = "string"
-  default = ""
+  description = "Project name, will be added for resource tagging."
+  type        = string
+  default     = ""
 }
 
 variable "aws_region" {
-  type        = "string"
   description = "The Amazon region"
+  type        = string
 }
 
 variable "cidr_block" {
-  default = "10.0.0.0/16"
+  description = "The CIDR block used for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
-  type = "map"
-
-  default = {
-    us-east-1      = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
-    us-east-2      = ["us-east-2a", "eu-east-2b", "eu-east-2c"]
-    us-west-1      = ["us-west-1a", "us-west-1c"]
-    us-west-2      = ["us-west-2a", "us-west-2b", "us-west-2c"]
-    ca-central-1   = ["ca-central-1a", "ca-central-1b"]
-    eu-west-1      = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-    eu-west-2      = ["eu-west-2a", "eu-west-2b"]
-    eu-central-1   = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-    ap-south-1     = ["ap-south-1a", "ap-south-1b"]
-    sa-east-1      = ["sa-east-1a", "sa-east-1c"]
-    ap-northeast-1 = ["ap-northeast-1a", "ap-northeast-1c"]
-    ap-southeast-1 = ["ap-southeast-1a", "ap-southeast-1b"]
-    ap-southeast-2 = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
-    ap-northeast-1 = ["ap-northeast-1a", "ap-northeast-1c"]
-    ap-northeast-2 = ["ap-northeast-2a", "ap-northeast-2c"]
-  }
+  description = "List to specify the availability zones for which subnes will be created. By default all availability zones will be used."
+  type        = list
+  default     = []
 }
 
 variable "create_private_subnets" {
-  default = "true"
+  description = "Indicates to create private subnets."
+  type        = bool
+  default     = true
 }
 
 variable "create_private_hosted_zone" {
-  default = "true"
+  description = "Indicate to create a private hosted zone."
+  type        = bool
+  default     = true
 }
 
 variable "public_subnet_map_public_ip_on_launch" {
-  default = "false"
+  description = "Enable public ip creaton by default on EC2 instance launch."
+  type        = bool
+  default     = false
 }
 
 variable "create_s3_vpc_endpoint" {
-  default     = "true"
   description = "Whether to create a VPC Endpoint for S3, so the S3 buckets can be used from within the VPC without using the NAT gateway."
+  type        = bool
+  default     = true
 }
 
 variable "tags" {
-  type        = "map"
   description = "Map of tags to apply on the resources"
+  type        = map(string)
   default     = {}
 }
+
